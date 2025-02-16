e7ede49a976ef83088917632cfc2a5737d22de2b
mpaul7 <mpaul7@gmail.com>
2025-02-16 17:54:35 -0500

    dvc init2

--- src/dlProject/config/configuration.py
+++ src/dlProject/config/configuration.py
@@ -1,4 +1,5 @@
 from pathlib import Path
+from dlProject import logger
 from dlProject.constants import *
 from dlProject.utils.common import read_yaml, create_directories
 from dlProject.utils.features import *
@@ -29,6 +30,7 @@ class ConfigurationManager:
 
     def get_data_ingestion_config(self) -> DataIngestionConfig:
         config_data_ingestion = self.config.data_ingestion
+        logger.info(f"Data ingestion config: {config_data_ingestion}")
         create_directories([config_data_ingestion.root_dir])
 
         data_ingestion_config = DataIngestionConfig(
